2f3d4b2a0c9cca44877564cd67fcb7dda6e43f7d
Canorus <helios7157@gmail.com>
2018-07-31 15:41:51 +0900

    fix character overlapping issue

--- misemonzibot.py
+++ misemonzibot.py
@@ -7,7 +7,7 @@ def getimg(rg):
     imgdata = requests.get('http://www.webairwatch.com/kaq/modelimg_case4/PM'+rg+'KM.Animation.gif').content
     with open(rg+'.gif', 'wb') as handler:
         handler.write(imgdata)
-    os.system('convert '+rg+'.gif -fuzz 10% -transparent white '+rg+'.gif')
+    os.system('convert -dispose previous '+rg+'.gif -fuzz 10% -transparent white '+rg+'.gif')
 
 getimg('10.09')
 getimg('2_5.09')
